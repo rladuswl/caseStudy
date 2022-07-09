069a7cd7367b6ed587ba153df9f0d99c11aaff58
rladuswl <khkkgon18@naver.com>
2022-07-09 21:30:57 +0900

    컴포넌트 함수 만들기

--- first/src/App.js
+++ first/src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="product">
+      <img src="https://media.bunjang.co.kr/product/183206594_1_1654042021_w292.jpg" />
     </div>
+  </head>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ script.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductList = ({name, img}) => {
+  return (
+    <div class="product">
+      <img src="https://media.bunjang.co.kr/product/183206594_1_1654042021_w292.jpg" />
+    </div>
+  )
+}
+
+export default ProductList;
